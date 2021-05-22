edb7fe2a8f4c930427cc363dbae824f887a4be1c
JASS <apple@jasss-MacBook-Pro.local>
2021-05-23 03:40:11 +0400

    mapState and DispatchState has been completed

--- src/Containers/HomeContainer.js
+++ src/Containers/HomeContainer.js
@@ -1,3 +1,11 @@
+import { connect } from "react-redux";
 import Home from "../Components/Home";
+import { AddToCart } from "../Services/Actions/action";
 
-export default Home;
+const mapStateToProps = (state) => {};
+
+const mapDispatchToprops = (dispatch) => {
+  addToCartHandler: (data) => dispatch(AddToCart(data));
+};
+
+export default connect(mapStateToProps, mapDispatchToprops)(Home);
--- src/Services/Actions/action.js
+++ src/Services/Actions/action.js
@@ -1,4 +1,4 @@
-import { ADD_TO_CART } from "../constants";
+import { ADD_TO_CART, REMOVE_FROM_CART } from "../constants";
 
 {
   /* this action.js will take data from react and pass that to redux store so that store will perform accordingly. e.g.:- when user will click to add button, then store will add data and vise versa */
@@ -10,7 +10,6 @@ export const AddToCart = (data) => {
     data: data,
   };
 };
-
 
 export const RemoveFromCart = (data) => {
   return {
